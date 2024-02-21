c0380e46b303444722824ea2b12a4a371c6019ed
Gilaric <pkg.thomsen@gmail.com>
2024-02-21 12:32:56 +0100

    Race is made.

--- src/main/java/Project/TotalWar/Controller/RaceController.java
+++ src/main/java/Project/TotalWar/Controller/RaceController.java
@@ -3,10 +3,9 @@ package Project.TotalWar.Controller;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import Project.TotalWar.Model.LordModel;
-import Project.TotalWar.Repository.LordRepository;
+import Project.TotalWar.Model.RaceModel;
 import Project.TotalWar.Repository.RaceRepository;
-import Project.TotalWar.util.LordModelAssembler;
+import Project.TotalWar.util.RaceModelAssembler;
 import Project.TotalWar.util.NotFoundException;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
@@ -25,62 +24,58 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RestController
 public class RaceController {
     private final RaceRepository repository;
-    private final LordModelAssembler assembler;
+    private final RaceModelAssembler assembler;
 
-    LordController(LordRepository repository, LordModelAssembler assembler) {
+    RaceController(RaceRepository repository, RaceModelAssembler assembler) {
         this.repository = repository;
         this.assembler = assembler;
     }
 
     // Aggregate root
-    // tag::get-aggregate-root[]
-    @GetMapping("/lords")
-    public CollectionModel<EntityModel<LordModel>> all() {
+    @GetMapping("/races")
+    public CollectionModel<EntityModel<RaceModel>> all() {
 
-        List<EntityModel<LordModel>> lords = repository.findAll().stream() //
+        List<EntityModel<RaceModel>> races = repository.findAll().stream() //
                 .map(assembler::toModel) //
                 .collect(Collectors.toList());
 
-        return CollectionModel.of(lords, linkTo(methodOn(LordController.class).all()).withSelfRel());
+        return CollectionModel.of(races, linkTo(methodOn(RaceController.class).all()).withSelfRel());
     }
-    // end::get-aggregate-root[]
 
-    @PostMapping("/lord")
-    LordModel newLord(@RequestBody LordModel newLord) {
-        return repository.save(newLord);
+    @PostMapping("/race")
+    RaceModel newRace(@RequestBody RaceModel newRace) {
+        return repository.save(newRace);
     }
 
     // Single item
+    @GetMapping("/race/{id}")
+    public EntityModel<RaceModel> one(@PathVariable Long id) {
 
-    @GetMapping("/lord/{id}")
-    public EntityModel<LordModel> one(@PathVariable Long id) {
-
-        LordModel lord = repository.findById(id) //
+        RaceModel race = repository.findById(id) //
                 .orElseThrow(() -> new NotFoundException(id));
 
-        return assembler.toModel(lord);
+        return assembler.toModel(race);
     }
 
-    @PutMapping("/lord/{id}")
-    LordModel replaceLord(@RequestBody LordModel newLord, @PathVariable Long id) {
+    @PutMapping("/race/{id}")
+    RaceModel replaceRace(@RequestBody RaceModel newRace, @PathVariable Long id) {
 
         return repository.findById(id)
-                .map(lord -> {
-                    lord.setLordName(newLord.getLordName());
-                    lord.setFaction(newLord.getFaction());
-                    lord.setFactionLeader(newLord.getFactionLeader());
-                    lord.setRace(newLord.getRace());
+                .map(race -> {
+                    race.setRaceName(newRace.getRaceName());
+                    race.setFaction(newRace.getFaction());
+                    race.setHero(newRace.getHero());
 
-                    return repository.save(lord);
+                    return repository.save(race);
                 })
                 .orElseGet(() -> {
-                    newLord.setLordId(id);
-                    return repository.save(newLord);
+                    newRace.setRaceId(id);
+                    return repository.save(newRace);
                 });
     }
 
-    @DeleteMapping("/lord/{id}")
-    void deleteEmployee(@PathVariable Long id) {
+    @DeleteMapping("/race/{id}")
+    void deleteRace(@PathVariable Long id) {
         repository.deleteById(id);
     }
 }
